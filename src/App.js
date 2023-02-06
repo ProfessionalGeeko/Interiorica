da1c3bb4020a46d1332af1d40918d8bf8a668b8d
Pranjal Singh <80697977+ProfessionalGeeko@users.noreply.github.com>
2023-02-06 17:53:34 +0530

    Revert "Dev_Satyam_interiorica"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import Homepage from './pages/Homepage/homepage';
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      Interiorica App
     </div>
   );
 }
