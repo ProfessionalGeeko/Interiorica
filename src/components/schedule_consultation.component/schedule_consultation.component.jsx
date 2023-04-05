d4cd61c84febf491a4b86a2351b0a5f9c6d99413
Satyam Shukla <satyamshukla95183@gmail.com>
2023-04-05 15:42:49 +0530

    modified the code to make it more responsive

--- src/components/schedule_consultation.component/schedule_consultation.component.jsx
+++ src/components/schedule_consultation.component/schedule_consultation.component.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+import { Box, Button, Container, Typography } from '@mui/material';
+import { styled } from '@mui/system';
 import  './schedule_consultation.styles.scss';
 
+const QuestionContainer = styled(Container)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100%',
+    padding: '10rem',
+});
+
+const StyledButton = styled(Button)({
+  marginTop: '1rem',
+  width: '100%',
+  maxWidth: 300,
+});
+
 export default function Projects({data, image}) {
-    return (
-            <div className='project_wrapper'>
-                <div className='consult_descr'>
-                    <p>Ready to get started? Contact us today to make your design dreams a reality.</p>
-                </div>
-                <Button variant="contained" size="medium"  className="Schedule_consult">Schedule a Consultation</Button>
-            </div>
-    );
-}
+  return (
+    <QuestionContainer maxWidth="sm">
+      <Typography variant="h4" align="center" gutterBottom >
+      Ready to get started? 
+      </Typography>
+      <Typography variant="body1" align="center" gutterBottom sx={{ mt: 1, mb:4 }}>
+      Contact us today to make your design dreams a reality.
+      </Typography>
+      <StyledButton variant="contained" className="Schedule_consult">
+        Schedule a consultation
+      </StyledButton>
+    </QuestionContainer>
+  );
+};
+
+
