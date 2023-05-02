577b96b5111bfe8b1aa7181474f183bc018b90b4
Pranjal Singh <pranjal366@gmail.com>
2023-05-02 21:24:24 +0530

    Integrated Customer Review API

--- src/components/customer-review/CustomerReview.js
+++ src/components/customer-review/CustomerReview.js
@@ -17,33 +17,35 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const images = [
-    {
-        label: 'Project Name',
-        imgPath:
-            'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Project Name',
-        imgPath:
-            'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Project Name',
-        imgPath:
-            'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-    },
-    {
-        label: 'Project Name',
-        imgPath:
-            'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-];
+// const images = [
+//     {
+//         label: 'Project Name',
+//         imgPath:
+//             'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+//     },
+//     {
+//         label: 'Project Name',
+//         imgPath:
+//             'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+//     },
+//     {
+//         label: 'Project Name',
+//         imgPath:
+//             'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+//     },
+//     {
+//         label: 'Project Name',
+//         imgPath:
+//             'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+//     },
+// ];
 
 function SwipeableTextMobileStepper() {
     const theme = useTheme();
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-    const [activeStep, setActiveStep] = React.useState(0);
+    const [activeStep, setActiveStep] = useState(0);
+    const [images, setImages] = useState([]);
+    const { isLoading, sendRequest } = useHttp();
     const maxSteps = images.length;
 
     const handleNext = () => {
@@ -58,6 +60,14 @@ function SwipeableTextMobileStepper() {
         setActiveStep(step);
     };
 
+    const handleUserReviewsData = (data) => {
+        setImages(data);
+    }
+
+    useEffect(() => {
+        sendRequest({method: 'GET', url: '/get-data?collectionName=userReviews'}, handleUserReviewsData)
+    }, [])
+
     return (
         <Box backgroundColor={`${prefersDarkMode ? 'rgb(30,32,37)' : '#DFDFDE'}`} px={8} py={4} sx={{ flexGrow: 1 }}>
             <Paper
@@ -70,7 +80,7 @@ function SwipeableTextMobileStepper() {
                     backgroundColor: `${prefersDarkMode ? 'rgb(30,32,37)' : '#DFDFDE'}`
                 }}
             >
-                <Typography fontWeight="bold" >{images[activeStep].label}</Typography>
+                <Typography fontWeight="bold" >{images.length ? images[activeStep].projectName: 'No'}</Typography>
             </Paper>
             <AutoPlaySwipeableViews
                 axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
@@ -79,7 +89,7 @@ function SwipeableTextMobileStepper() {
                 enableMouseEvents
             >
                 {images.map((step, index) => (
-                    <div key={`${step.label}-${index}`}>
+                    <div key={`${step.projectName}-${index}`}>
                         {Math.abs(activeStep - index) <= 2 ? (
                             <>
                                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
@@ -93,16 +103,16 @@ function SwipeableTextMobileStepper() {
                                                 overflow: 'hidden',
                                                 width: '100%',
                                             }}
-                                            src={step.imgPath}
-                                            alt={step.label}
+                                            src={step.clientImage}
+                                            alt={step.projectName}
                                         />
                                     </Grid>
                                     <Grid px={4} item md={7} sm={8} xs={4} my="auto">
                                         <Typography color={`${prefersDarkMode ? 'white' : 'black'}`} align="justify" component="p" variant="p">
-                                            Our satisfied clients keep us alive and prospering. We are happy to be able to provide all of our clients with unparalleled services. We’re pleased to say that due to our efforts, our clients always come back to us for more. Read below to find out all the wonderful things our clients have to say about us. We are sure you will soon be convinced to give us a call.
+                                            {step.clientFeedback}
                                         </Typography>
                                         <Typography fontStyle="italic" color={`${prefersDarkMode ? 'white' : 'black'}`}  component="p" variant="p" align="right">
-                                            - Client Name
+                                            {`--- ${step.clientName}`}
                                         </Typography>
                                     </Grid>
                                 </Grid>
