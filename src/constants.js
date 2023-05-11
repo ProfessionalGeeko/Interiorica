cea0ef05c7a1bcc828ec1b22ad0b8fa3cbb4f719
Pranjal Singh <pranjal366@gmail.com>
2023-05-11 18:58:32 +0530

    Updated Text Content

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
 /* Intro Page Constants */
-export const introTitle = 'Interiorica ';
+export const introTitle = "Singh's Interiors";
 export const introSubtitle = 'Your Space. Reimagined.';
 export const introDescription = 'At Interiorica  we design residential and commercial spaces that are comfortable, personal and interesting. Great interior design is great art, reflecting who you are in ways you never imagined. Our unique perspective, grounded in both art and architecture, reflects an artist’s eye toward color and materials and a strong sense of space and form. We are artists, we are designers, and we are here to make your space better than you could have ever imagined.';
 
 /* About Us Page Constants */
 export const aboutUsTitle = 'About Us';
-export const aboutUsDescriptionOne = 'Bring your vision to life with Interiorica. Since 2019, our professional, creative team has been designing the spaces that are most important to you. From our initial strategy shopping session to laying down the rugs on your new floors, we’ll lead the way with expert guidance.';
+export const aboutUsDescriptionOne = "Bring your vision to life with Singh's Interiors. Since 2016, our professional, creative team has been designing the spaces that are most important to you. From our initial strategy shopping session to laying down the rugs on your new floors, we’ll lead the way with expert guidance.";
 export const aboutUsDescriptionTwo = 'No matter the size of the project, the goal is always the same: to make your personal style shine. We provide new inspiration and ideas, while incorporating elements of your liking into every concept we present. Schedule a consultation with us today and feel the Interiorica difference.';
 
 /* Residential Design Page Constants */
@@ -15,13 +15,12 @@ export const residentialDesignContent = 'Sometimes the little things make all th
 
 /* Commercial Design Page Constants */
 export const commercialDesignHeader = 'Commercial Design';
-export const commercialDesignSubHeader = 'The Little Touches that Make All the Difference';
-export const commercialDesignContent = 'Sometimes the little things make all the difference. With our Pre-Construction Consultation services, we’ll be able to transform your space and set the mood. Whatever your interior design needs may be, our team of experts and architects are here to make sure you feel at home in the space you live or work in. Contact us today and get started.';
-
+export const commercialDesignSubHeader = 'The Only Right Things that Brings All the Difference';
+export const commercialDesignContent = 'We specialize in commercial interiors, from single-room makeovers to full-scale renovations and new construction. We offer a wide range of services, including space planning, color consulting, furniture selection, lighting design, custom cabinetry, and art curation. Our expertise in materials, textures, and finishes allows us to create unique and timeless designs that stand the test of time.';
 /* Project Youtube Page Constants */
 export const projectPageTitle = 'Projects';
-export const projectPageSubtitle = 'The Little Touches that Make All the Difference';
-export const projectPageDescription = 'Sometimes the little things make all the difference. With our Pre-Construction Consultation services, we’ll be able to transform your space and set the mood. Whatever your interior design needs may be, our team of experts and architects are here to make sure you feel at home in the space you live or work in. Contact us today and get started.';
+export const projectPageSubtitle = 'The Actual Showcase of work taken by Us.';
+export const projectPageDescription = 'We are committed to sustainability and ethical practices, and we source our materials and products from local and eco-friendly suppliers whenever possible. We also stay up-to-date with the latest trends and technologies in the industry, and we attend conferences and trade shows to expand our knowledge and network.';
 
 /* Consultation Page Constants */
 export const consultationTitle = 'Ready to get started?';
@@ -35,5 +34,5 @@ export const reviewContent = 'Our satisfied clients keep us alive and prospering
 /* Contact Us Page Constants */
 export const contactUsTitle = 'Contact Us';
 export const contactUsDescription = 'The way to incredible spaces that reflect your personality are only a call away. Interested in working with us? Get in touch today.';
-export const contactUsAddress = 'A/Shop No 14, Krishna Kunj Building, 150 feet Road, Bhayandar (W), Maharashtra 401101.';
-export const contactUsEmail = 'singhsinterior.info@gmail.com';
+export const contactUsAddress = 'Neeladri Rd, Neeladri Nagar, Electronics City Phase 1, Doddathoguru, Bengaluru, Karnataka 560100';
+export const contactUsEmail = 'singhsinteriors@gmail.com';
